727acef1815cc201cea9999a5355ced9f0ba815e
Cerberus-Dev <hod.cerberus@gmail.com>
2025-09-08 13:31:09 +0300

    Add files via upload

---
+++ src/Cerberus_kernel.c
@@ -0,0 +1,23 @@
+/*
+ * Cerberus OS - Triple-Layer Secure Operating System
+ * Developer: Hod
+ * Year: 2025
+ * GitHub: github.com/HodOS/CerberusOS
+ * License: MIT
+ * Copyright (c) 2025 Hod. All rights reserved.
+ */
+
+#include <stdio.h>
+
+int cerberus_init() {
+    printf("[CERBERUS] Kernel initializing...\n");
+    printf("[CERBERUS] AI Guardian: ONLINE\n");
+    printf("[CERBERUS] Quantum Encryption: ACTIVE\n");
+    printf("[CERBERUS] Process Isolation: SECURED\n");
+    printf("[CERBERUS] All three heads are watching. System secure.\n");
+    return 0;
+}
+
+int main() {
+    return cerberus_init();
+}
